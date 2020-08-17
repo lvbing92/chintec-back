515f2cc5a25337739cb6da20f9ec66cd7a494fb9
tangxinli <jeff.li@chintec.net>
2020-08-17 16:53:11 +0800

    author: jeff content: init project 05 date:2020年8月17日16:32:05

--- chintec-auth/src/main/java/com/chintec/auth/AuthApplication.java
+++ chintec-auth/src/main/java/com/chintec/auth/AuthApplication.java
@@ -2,7 +2,6 @@ package com.chintec.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 /**
  * @author Jeff·Tang
--- chintec-auth/src/test/java/com/chintec/auth/RedisTest.java
+++ chintec-auth/src/test/java/com/chintec/auth/RedisTest.java
@@ -8,9 +8,7 @@ import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
-import java.util.Arrays;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**

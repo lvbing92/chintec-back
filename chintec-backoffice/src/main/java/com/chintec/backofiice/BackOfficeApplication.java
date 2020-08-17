2099f30bf82f499fe1c9cf2594d70a1bfd05c32b
tangxinli <jeff.li@chintec.net>
2020-08-17 16:32:13 +0800

    author: jeff content: init project 02 date:2020年8月17日16:32:05

---
+++ chintec-backoffice/src/main/java/com/chintec/backofiice/BackOfficeApplication.java
@@ -0,0 +1,16 @@
+package com.chintec.backofiice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jeff·Tang
+ * @version 1.0
+ * @date 2020/8/17 16:28
+ */
+@SpringBootApplication
+public class BackOfficeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackOfficeApplication.class,args);
+    }
+}
---
+++ chintec-miniprogram/src/main/java/com/chintec/miniprogram/MiniprogramApplication.java
@@ -0,0 +1,16 @@
+package com.chintec.miniprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jeff·Tang
+ * @version 1.0
+ * @date 2020/8/17 16:30
+ */
+@SpringBootApplication
+public class MiniprogramApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MiniprogramApplication.class, args);
+    }
+}

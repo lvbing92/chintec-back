93cdefcff1a323c5ee1cc210401d378bcf478a62
tangxinli <jeff.li@chintec.net>
2020-08-18 10:32:13 +0800

    author: jeff content: init project: order,message,common,generator date:2020年8月17日16:32:05

---
+++ chintec-generator/src/main/java/com/chintec/generator/MyGenerator.java
@@ -0,0 +1,13 @@
+package com.chintec.generator;
+
+
+/**
+ * @author Jeff·Tang
+ * @version 1.0
+ * @date 2020/6/17 9:43
+ */
+public class MyGenerator {
+    public static void main(String[] args) {
+    GeneratorConfig.getMyGenerator("class_pack","class_pack","Jeff·Tang");
+    }
+}

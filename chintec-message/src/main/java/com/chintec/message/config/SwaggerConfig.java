9bccc6daf232b3011381bf28d7fabbfac0911fee
Jack <jack.he@chintec.net>
2020-08-26 13:10:33 +0800

    oss upload img

--- chintec-message/src/main/java/com/chintec/message/config/SwaggerConfig.java
+++ chintec-message/src/main/java/com/chintec/message/config/SwaggerConfig.java
@@ -1,18 +1,14 @@
 package com.chintec.message.config;
 
-import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static com.google.common.base.Predicates.or;
-import static springfox.documentation.builders.PathSelectors.regex;
-
 /**
  * @author Jack
  */
@@ -26,6 +22,7 @@ public class SwaggerConfig {
      */
     @Bean
     public Docket controllerApi() {
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(new ApiInfoBuilder()
                         .title("标题:Message管理")
@@ -35,12 +32,9 @@ public class SwaggerConfig {
                         .build())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.chintec.message"))
+                .paths(PathSelectors.any())
                 .build();
-    }
 
-    @SuppressWarnings("unchecked")
-    private Predicate<String> paths() {
-        return or(regex("/user/api/.*?"));
     }
 
 }

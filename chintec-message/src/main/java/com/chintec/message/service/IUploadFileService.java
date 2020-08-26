277bc5eb1e9f2685a510033cf3dc54b41564d177
Jack <jack.he@chintec.net>
2020-08-26 11:21:42 +0800

    oss upload img

---
+++ chintec-message/src/main/java/com/chintec/message/service/IUploadFileService.java
@@ -0,0 +1,23 @@
+package com.chintec.message.service;
+
+import com.chintec.common.util.ResultResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author Jack
+ * @version 1.0
+ * @date 2020/7/6 13:08
+ */
+public interface IUploadFileService {
+
+
+    /**
+     * 保存图片
+     *
+     *
+     *
+     * @param file
+     * @return
+     */
+    ResultResponse uploadImg2Oss(MultipartFile file);
+}
